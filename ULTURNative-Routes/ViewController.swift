16e0ddf83bb9478e9611ea6bec7b2ad1117fdab7
LIANG ZHAO <liangzhao@LIANGs-MacBook-Pro.local>
2017-10-12 19:49:34 -0700

    add speed features

--- ULTURNative-Routes/ViewController.swift
+++ ULTURNative-Routes/ViewController.swift
@@ -8,12 +8,47 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate
+{
+    //this is map view
+    @IBOutlet weak var map: MKMapView!
+    
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations[0]
+        //set the zoom
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01,0.01)
+        
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        //calculation convert location object to double type
+        let vspeed=Double(location.speed)
+        let speedh=round(vspeed*3.6)
+        speedLabel.text=String(speedh)+"  Km/H"
+        
+        self.map.showsUserLocation = true
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
